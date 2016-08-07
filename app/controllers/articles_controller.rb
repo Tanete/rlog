64d1b0edb7b7f819aac37b50b5c60c6cb6d7b174
tanete <tanete772@outlook.com>
2016-08-07 10:59:14 +0800

    Test article page for auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
+  before_filter :authorize
+
   def index
     @articles = Article.all
   end
