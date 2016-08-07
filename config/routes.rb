8b8531c527ce0863587f63763ef0d19deac705d5
tanete <tanete772@outlook.com>
2016-08-07 10:57:53 +0800

    Session auth Wokrs now.
    form_for, model; for_tag, simple form.
    http://stackoverflow.com/questions/1349348/difference-between-form-for-form-tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'sign_up' => 'users#new', :as => 'sign_up'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get 'sign_up' => 'users#new'
   resources :users
-  
+
   get 'welcome/index'
 
   resources :articles do
